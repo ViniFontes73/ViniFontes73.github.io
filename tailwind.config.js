a81f070d1c8c09b30fedec81b1e27655ac44f67f
Vinicius <viniciusfontesads@gmail.com>
2023-11-04 20:03:55 -0300

    testando tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,13 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      backgroundColor: {
+        '526ca8': '#526ca8',
+      },
+    },
+  },
+  plugins: [],
+}
+
