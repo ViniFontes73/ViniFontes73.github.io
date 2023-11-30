8de2a1423b363574521a05f5db59058163a2f414
Vinicius <viniciusfontesads@gmail.com>
2023-11-30 19:04:00 -0300

    Adicionando vídeoe testando

---
+++ script.js
@@ -0,0 +1,10 @@
+function abrirVideo() {
+    document.querySelector('.overlay').style.display = 'block';
+    document.querySelector('.modal').style.display = 'block';
+  }
+  
+  function fecharVideo() {
+    document.querySelector('.overlay').style.display = 'none';
+    document.querySelector('.modal').style.display = 'none';
+  }
+  
